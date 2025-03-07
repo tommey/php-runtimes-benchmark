5cc594bc3035bf44d392f3a594bd20f07abd10a6
tommey <github@tommey.hu>
2025-03-07 14:11:21 +0100

    Added optional busy logic to simulate real-world apps with http calls etc

--- 001_symfony7_wo_db/project/src/Controller/FrontpageController.php
+++ 001_symfony7_wo_db/project/src/Controller/FrontpageController.php
@@ -13,14 +13,28 @@ class FrontpageController extends AbstractController
     public function index(
         #[MapQueryParameter] string $firstName = '',
         #[MapQueryParameter] string $lastName = '',
+        #[MapQueryParameter] string $busy = 'no',
     ): Response
     {
+        if ($busy !== 'no') {
+            $busy = '';
+            for ($i = random_int(100, 1000); $i > 0; $i--) {
+                $busy .= random_bytes(random_int(1, 5));
+            }
+            $max = random_int(1, 10000) === 1 ? 1000000 : 100000;
+            usleep(random_int(10000, $max));
+            for ($i = random_int(1000, 10000); $i > 0; $i--) {
+                $busy .= random_bytes(random_int(1, 5));
+            }
+        }
+
         return $this->render(
             'frontpage.html.twig',
             [
                 'first_name' => $firstName,
                 'last_name' => $lastName,
                 'hrtime' => hrtime(true),
+                'busy' => $busy === 'no' ? $busy : md5($busy),
             ]
         );
     }
